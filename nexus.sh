171e57623dd7aee013fe3cfdc064641beb13fb47
Neha Pyla <113757428+nehapyla@users.noreply.github.com>
2026-06-27 16:57:58 +0530

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -2,8 +2,8 @@ sudo yum update -y
 sudo yum install wget -y
 sudo yum install java-21-amazon-corretto-jmods -y
 sudo mkdir /app && cd /app
-sudo wget https://download.sonatype.com/nexus/3/nexus-3.79.1-04-linux-x86_64.tar.gz
-sudo tar -xvf nexus-3.79.1-04-linux-x86_64.tar.gz
+sudo wget https://download.sonatype.com/nexus/3/nexus-3.93.2-01-linux-x86_64.tar.gz
+sudo tar -zxvf nexus-3.93.2-01-linux-x86_64.tar.gz
 sudo mv nexus-3.79.1-04 nexus
 sudo adduser nexus
 sudo chown -R nexus:nexus /app/nexus
